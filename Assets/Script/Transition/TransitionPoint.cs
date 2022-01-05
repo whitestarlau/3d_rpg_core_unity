271da59edf72edab4ae7003960dc4ec294bb7bbd
liuyixin.star <liuyixin.star@bytedance.com>
2022-01-05 11:28:58 +0800

    30:Create Protal.

---
+++ Assets/Script/Transition/TransitionPoint.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionPoint : MonoBehaviour
+{
+    public enum TransitionType
+    {
+        SameScene, DifferentScene
+    }
+    [Header("Transition Info")]
+    public string sceneName;
+
+    public TransitionType transitionType;
+
+    public TransitionDestination.DesinationTag desinationTag;
+
+    private bool canTrans;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            canTrans = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            canTrans = false;
+    }
+}
